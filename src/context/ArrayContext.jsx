2b9fc3e36303f266e1472667c9e5dc1e30b2db5a
Satyam Jha <satyam@Satyams-MacBook-Pro.local>
2023-11-12 13:51:17 +0530

    session storage

--- src/context/ArrayContext.jsx
+++ src/context/ArrayContext.jsx
@@ -1,10 +1,30 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 export const ListDataContext = createContext();
 
 export const ListDataContextProvider = ({ children }) => {
 
     const [list, setList] = useState([])
+
+    useEffect(() => {
+
+        if (list.length > 0) sessionStorage.setItem("list", JSON.stringify(list))
+    }, [list])
+
+    useEffect(() => {
+
+        const sessionList = sessionStorage.getItem("list");
+
+        function updateList() {
+
+            const parsedList = JSON.parse(sessionList)
+            setList(parsedList)
+
+        }
+
+        sessionList && updateList()
+
+    }, [])
 
 
     return (
